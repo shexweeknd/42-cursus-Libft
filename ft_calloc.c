df54d3b7c91898ee2a10da6765e109a22cac6057
Herinambinintsoa Nantenaina Ramarosandratana <hramaros@c1r11s5.42antananarivo.mg>
2024-02-22 14:27:25 +0300

    ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,13 +6,23 @@
 /*   By: hramaros <hramaros@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 09:45:44 by hramaros          #+#    #+#             */
-/*   Updated: 2024/02/22 13:43:56 by hramaros         ###   ########.fr       */
+/*   Updated: 2024/02/22 14:27:09 by hramaros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
+*/*
+void	ft_bzero(void *s, size_t n)
+{
+	int	i;
+
+	i = 0;
+	while (n--)
+		*(unsigned char *)(s + i++) = 0;
+}
+*/
 void	*ft_calloc(size_t n, size_t size)
 {
 	char	*ret;
@@ -20,6 +30,7 @@ void	*ft_calloc(size_t n, size_t size)
 
 	total = n * size;
 	ret = (void *)malloc(total);
+	ft_bzero(ret, total);
 	if (!ret || total < -2147483648 || total > 2147483647)
 		return (0);
 	return (ret);
@@ -27,18 +38,22 @@ void	*ft_calloc(size_t n, size_t size)
 
 /*
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include <unistd.h>
 
 int main (void)
 {
-	char *str;
+	int size = 8539;
+	int ret;
+	void *str;
+	void *test;
 
-	str = (char *)calloc(30, 1);
-	if (!str)
-		write(1, "NULL", 4);
-	else
-	{
-		write(1, str, 30);
-	}
+	str = (char *)ft_calloc(size, sizeof(int));
+	test = (char *)calloc(size, sizeof(int));
+	ret = memcmp(str, test, size * sizeof(int));
+	printf("%d\n", ret);
+	free(str);
+	free(test);
 	return (0);
 }*/
