44b92ec3f07f8b414967e37c16d100e099e36093
Herinambinintsoa Nantenaina Ramarosandratana <hramaros@c1r11s5.42antananarivo.mg>
2024-02-22 15:42:32 +0300

    ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,46 +6,45 @@
 /*   By: hramaros <hramaros@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 16:54:27 by hramaros          #+#    #+#             */
-/*   Updated: 2024/02/22 15:06:14 by hramaros         ###   ########.fr       */
+/*   Updated: 2024/02/22 15:42:14 by hramaros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <stdio.h>
 
 char	*ft_strnstr(const char *str, const char *to_find, size_t n)
 {
-	int	i;
+	size_t	i;
+	size_t	j;
+	size_t	to_find_len;
 
-	if (!*(unsigned char *)to_find)
+	if (ft_strlen(to_find) == 0)
 		return ((char *)str);
-	if (!*(unsigned char *)str)
-		return (NULL);
+	to_find_len = ft_strlen(to_find);
 	i = 0;
-	while (n--)
+	while (i < n && str[i])
 	{
-		if (*(unsigned char *)str != *(unsigned char *)(to_find + i))
-			i = 0;
-		if (*(unsigned char *)str
-			&& *(unsigned char *)str++ == *(unsigned char *)(to_find + i))
-		{
-			if (*(unsigned char *)(to_find + i + 1) == '\0')
-				return ((char *)(--str - i));
-			i++;
-		}
+		j = 0;
+		while (str[i + j] && str[i + j] == to_find[j] && (i + j) < n)
+			j++;
+		if (j == to_find_len)
+			return (&((char *)str)[i]);
+		i++;
 	}
 	return (NULL);
 }
 
-/*
+
 #include <stdio.h>
 
 int	main(void)
 {
 	char *ret;
-	char *str = "";
-	char *to_find = "teste";
+	char *str = "lorem ipsum dol is amet dolor";
+	char *to_find = "dolor";
 
-	ret = ft_strnstr(str, to_find, 0);
+	ret = ft_strnstr(str, to_find, 15);
 	printf("%s\n", ret);
 	return (0);
-}*/
+}
