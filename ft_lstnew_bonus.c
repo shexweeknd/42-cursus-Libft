2aee8c549dcb9a28ae24743101cb7c9a4f582b44
shexweeknd <ramarosandratananantenaina@gmail.com>
2024-02-24 17:16:11 +0100

    ft_lstnew

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hramaros <hramaros@student.42antananari    +#+  +:+       +#+        */
+/*   By: tiny <tiny@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 14:47:32 by hramaros          #+#    #+#             */
-/*   Updated: 2024/02/24 17:13:31 by hramaros         ###   ########.fr       */
+/*   Updated: 2024/02/24 17:15:53 by tiny             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ t_list	*ft_lstnew(void *content)
 		return (0);
 	buffer = (t_list *)malloc(sizeof(t_list));
 	if (!buffer)
-		return (NULL);
+		return (0);
 	buffer->content = content;
-	buffer->next = NULL;
+	buffer->next = 0;
 	return (buffer);
 }
